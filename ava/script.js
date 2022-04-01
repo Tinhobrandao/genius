bda38f141b8dfdad56a51f7401f4e07fc5155a71
Victor Brandão <victor.brandao596@gmail.com>
2022-04-01 19:02:52 -0300

    Atualização do script e classes, só falta ordenar as cores

--- ava/script.js
+++ ava/script.js
@@ -1,138 +1,145 @@
 // DADOS DO JOGO
-const pcMoves = []
-const playerMoves = []
+let pcMoves = []
+let playerMoves = []
 const startButton = document.getElementById('start').addEventListener('click', getPcMoves)
 const gameStats = document.createElement('section')
 gameStats.classList.add('gameStats')
 document.querySelector('.container').appendChild(gameStats)
 
-const phaseLevel = 1
+let phaseLevel = 0
 const phaseLevelDisplay = document.createElement('p')
-phaseLevelDisplay.classList.add('levelDisplay')
 phaseLevelDisplay.innerText = `Fase: ${phaseLevel}`
+phaseLevelDisplay.classList.add('levelDisplay')
 gameStats.appendChild(phaseLevelDisplay)
 let score = 0
+
 const scoreDisplay = document.createElement('p')
-scoreDisplay.classList.add('scoreDisplay')
 scoreDisplay.innerText = `Pontuação: ${score}`
+scoreDisplay.classList.add('scoreDisplay')
 gameStats.appendChild(scoreDisplay)
 let numeroSorteado = 0
 let interval = 0
 
-
-//  EventLinsteners das Cores p/ gravar o click e armazenar onde foi no array playersMoves
-
-const greenOne = document.querySelector('.verde')
-const redOne = document.querySelector('.vermelho')
-const blueOne = document.querySelector('.azul')
-const yellowOne = document.querySelector('.amarelo')
-
-greenOne.addEventListener('click', event => {
-    playerMoves.push(0)
-    waitToCheck()
-})
-
-redOne.addEventListener('click', event => {
-    playerMoves.push(1)
-    waitToCheck()
-})
-
-blueOne.addEventListener('click', event => {
-    playerMoves.push(2)
-    waitToCheck()
-})
-
-yellowOne.addEventListener('click', event => {
-    playerMoves.push(3)
-    waitToCheck()
-})
+addEventToPads()
 
 
 // Função para pegar um número aleatório de 0 a 3 e armazenar no array pcMoves
 function getPcMoves() {
+    playerMoves = []
     const getAnumber = Math.floor(Math.random() * 4)
-    pcMoves.push(getAnumber)
-    colorChoose(getAnumber)
-}
-
-
-
-function waitToCheck() {
-    if (pcMoves.length !== playerMoves.length) {
-        null
-    } else {
-        verifyMoves(pcMoves, playerMoves)
+    if (getAnumber === 0) {
+        pcMoves.push('verde')
     }
+    if (getAnumber === 1) {
+        pcMoves.push('vermelho')
+    }
+    if (getAnumber === 2) {
+        pcMoves.push('azul')
+    }
+    if (getAnumber === 3) {
+        pcMoves.push('amarelo')
+    }
+    colorChoose(pcMoves)
 }
 
 
-// Função para acender a cor que o PC escolher
-function colorChoose(getPcMoves) {
+function lightPads(pad, padColor) {
+    pad.classList.add(`${padColor}--active`)
+    setTimeout(function() {
+        pad.classList.remove(`${padColor}--active`)
+    }, 100)
+}
 
-    // const sounds = document.querySelectorAll('audio')
-    pcMoves.forEach((elemen, index, array) => {
-        setTimeout(() => {
-            if (array[index] === 0) {
-                greenOne.classList.toggle('verde--active')
-                    // sounds[index].play()
-            }
-        }, 1000)
+function pcColorLights(pad, padColor) {
+    setTimeout(() => {
+        pad.classList.add(`${padColor}--active`)
+    }, 1000)
+    setTimeout(function() {
+        pad.classList.remove(`${padColor}--active`)
+    }, 2000)
+}
 
-        setTimeout(() => {
-            if (array[index] === 1) {
-                redOne.classList.toggle('vermelho--active')
-                    // sounds[index].play()
-            }
-        }, 2000)
 
-        setTimeout(() => {
-            if (array[index] === 2) {
-                blueOne.classList.toggle('azul--active')
-                    // sounds[index].play()
-            }
-        }, 3000)
-
-        setTimeout(() => {
-            if (array[index] === 3) {
-                yellowOne.classList.toggle('amarelo--active')
-                    // sounds[index].play()
-            }
-        }, 4000)
-
-        cleanColors()
+function addEventToPads() {
+    const pads = [...document.querySelectorAll('.pad')]
+    pads.forEach(function(elem) {
+        elem.addEventListener('click', function(e) {
+            const pad = e.target
+            const padColor = pad.classList[1].split('-')[2]
+            playerMoves.push(padColor)
+            lightPads(pad, padColor)
+            waitToCheck()
+        })
     })
 }
 
-function cleanColors() {
-    setTimeout(() => {
-        greenOne.classList.remove('verde--active')
+function colorChoose(pcMoves) {
+    let padColor = ''
+    let pad = ''
+    pcMoves.forEach(function(element, index, array) {
+        if (array[index] === 'verde') {
+            padColor = 'verde'
+            pad = document.querySelector('.pad--verde')
+            setTimeout(() => {
+                pcColorLights(pad, padColor)
+            }, 500)
+        }
+        if (array[index] === 'vermelho') {
+            padColor = 'vermelho'
+            pad = document.querySelector('.pad--vermelho')
+            setTimeout(() => {
+                pcColorLights(pad, padColor)
+            }, 500)
+        }
+        if (array[index] === 'azul') {
+            padColor = 'azul'
+            pad = document.querySelector('.pad--azul')
+            setTimeout(() => {
+                pcColorLights(pad, padColor)
+            }, 500)
+        }
+        if (array[index] === 'amarelo') {
+            padColor = 'amarelo'
+            pad = document.querySelector('.pad--amarelo')
+            setTimeout(() => {
+                pcColorLights(pad, padColor)
+            }, 500)
+        }
 
-    }, 1500)
-    setTimeout(() => {
-        redOne.classList.remove('vermelho--active')
 
-    }, 2500)
-    setTimeout(() => {
-        blueOne.classList.remove('azul--active')
-
-    }, 3500)
-    setTimeout(() => {
-        yellowOne.classList.remove('amarelo--active')
-
-    }, 4500)
-
-
+    })
 }
 
+function waitToCheck() {
+    if (pcMoves.length === playerMoves.length) {
+        verifyMoves(pcMoves, playerMoves)
+
+    }
+}
+
+// function areArraysEqual(pcMoves, playerMoves) {
+//     if (pcMoves.length === playerMoves.length && pcMoves.every((value, index) => value === playerMoves[index])) {
+//         return true
+//     } else {
+//         return false
+//     };
+// }
+
 //Função para pausar o jogo, verificar se o jogador acertou os movimentos e reiniciar ou continuar.
-function verifyMoves() {
-    if (pcMoves === playerMoves) {
+function verifyMoves(pcMoves, playerMoves) {
+
+    if (pcMoves.toString() === playerMoves.toString()) {
         score++
         phaseLevel++
         const vitoria = document.createElement('h1')
         vitoria.innerText = 'Boa! Continue!'
         vitoria.classList.add('vitoria')
         document.querySelector('.container').appendChild(vitoria)
+        setTimeout(() => {
+            vitoria.remove('h1')
+        }, 2000)
+        scoreDisplay.innerText = `Pontuação: ${score}`
+        phaseLevelDisplay.innerText = `Fase: ${phaseLevel}`
         getPcMoves()
 
     } else {
@@ -142,10 +149,5 @@ function verifyMoves() {
         derrota.classList.add('derrota')
         const audioDerrota = new Audio('sounds/derrota.mp3')
         audioDerrota.play()
-
-
     }
-
-    console.log(pcMoves)
-    console.log(playerMoves)
 }
